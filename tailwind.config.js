b8ade3664facafab6cbf01ae8f270927a505f5a2
Kevin de Bie <kevin.de.bie@maxserv.com>
2023-02-22 10:58:40 +0100

    Add hover/active state to dice button

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
             "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'full': '0 0 30px rgba(0, 0, 0, 0.3)',
+      }
+    },
     colors: {
       'lightcyan': 'hsl(193, 38%, 86%)',
       'neongreen': 'hsl(150, 100%, 66%)',
